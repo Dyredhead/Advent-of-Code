ade77c3d9464f5ab4616785524fb67a484da8144
Dyredhead <danielyentin@gmail.com>
2022-12-03 23:57:42 -0500

    finished day 3

--- 2022/Python3.py
+++ 2022/Python3.py
@@ -1,5 +1,46 @@
 def main() -> None:
-    print("Hello, world!")
+    rucksacks = []
+    with open("input3.txt", 'r') as f:
+        rucksacks = f.read().strip().splitlines()
+    
+    # compartments = []
+    # for rucksack in rucksacks:
+    #     l = len(rucksack)//2
+    #     compartments.append((rucksack[:l], rucksack[l:]))
+
+    # sum = 0
+    # for i in compartments:
+    #     common = common_member2(i[0], i[1])
+    #     if common.islower():
+    #         sum += ord(common) - 96
+    #     else:
+    #         sum += ord(common) - 38
+    # print(sum)
+
+    sum = 0
+    i = 0
+    while (i < len(rucksacks)):
+        common = common_member3(rucksacks[i], rucksacks[i+1], rucksacks[i+2])
+        if common.islower():
+            sum += ord(common) - 96
+        else:
+            sum += ord(common) - 38
+        i += 3
+    print(sum)
+
+def common_member2(a, b):
+    a_set = set(a)
+    b_set = set(b)
+
+    return list(a_set & b_set)[0]
+
+def common_member3(a, b, c):
+    a_set = set(a)
+    b_set = set(b)
+    c_set = set(c)
+
+    return list(a_set & b_set & c_set)[0]
+
 
 if __name__ == "__main__":
 	main()
---
+++ 2022/Python4.py
@@ -0,0 +1,9 @@
+def main() -> None:
+    input = []
+    with open("input4.txt", 'r') as f:
+        input = f.read().strip().splitlines()
+    
+
+
+if __name__ == "__main__":
+	main()
