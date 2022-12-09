4f67130306a76dc08bf467b421efde6eb499da37
Dyredhead <danielyentin@gmail.com>
2022-12-09 14:35:49 -0500

    finished day 9

---
+++ 2022/AoC10.py
@@ -0,0 +1,13 @@
+def main() -> None:
+    input = []
+    with open("input.txt10", 'r') as f:
+        input = f.read().strip().splitlines()
+    
+    result = 0
+    for i in input:
+        continue
+
+    print(result)
+
+if __name__ == "__main__":
+	main()
